2934d0f1e09ec184e0ac4a27d18eeb3742d133b3
chikchicly <chikchicly@gmail.com>
2025-02-06 08:54:27 +0700

    menuList

--- menu/javascript-menu-for-landing.js
+++ menu/javascript-menu-for-landing.js
@@ -1,12 +1,20 @@
+// ------------------------------------------------------------------------------------------------------------------***------------------------------------------------------------------------------------------------------------------------------
+// path with menu -> const menuList.pathMenuList! this install all menu.
+// background menu it's menuList.background, that green/
+// if change link at menu -> menuList.ulol
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------/
+
 const menuList={
-    ulol : '<a href="#">menu</a><a href="#">tel:</a><a href="#">mail:</a>'
+    ulol : '<a href="#">menu</a><a href="#">tel:</a><a href="#">mail:</a>',
+    background : "green",
+    pathMenuList : "main"
 }
 window.addEventListener('DOMContentLoaded',menuFromLanding);
 function menuFromLanding(){
     let menuDiv=document.createElement('div'); /* maybe NAV */
     // menuDiv.innerHTML='menuOpen';
     menuDiv.setAttribute('id','menuDiv');
-    document.querySelector('body').prepend(menuDiv); /* body tag in which the menu is inserted. If you put main, then the menu wil be in the main tag or you can put it in the nav tag. */
+    document.querySelector(`${menuList.pathMenuList}`).prepend(menuDiv); /* body tag in which the menu is inserted. If you put main, then the menu wil be in the main tag or you can put it in the nav tag. */
     let menuCss=document.createElement('link');
     menuCss.setAttribute('rel','stylesheet');
     menuCss.setAttribute('href','menu/menuFromLanding.css');
@@ -30,35 +38,31 @@ function menuFromLanding(){
     function openMenu(){
         let sheetsMenu=document.createElement('div');
         sheetsMenu.classList.add('openMenu');
-        document.querySelector('body').prepend(sheetsMenu);
+        document.querySelector(`${menuList.pathMenuList}`).prepend(sheetsMenu);
         console.log('open');
             
-            
-            // document.querySelector('sheetsMenu').classList.add('openMenu');
         setTimeout(()=>{
             document.querySelector('.openMenu').style.cssText=`
                 height:100vh;
                 opacity:1;
                 font-size:33px;
                 `;
-            document.querySelector('.openMenu').style.background="green";
+            document.querySelector('.openMenu').style.background=`${menuList.background}`;
         },100);
         document.querySelector('.openMenu').innerHTML=`${menuList.ulol}`;
         document.querySelector('.openMenu').addEventListener('click',menuClose);
     }//menuOpen
     function menuClose(){
         document.querySelector('.openMenu').style.cssText=`
-                height:0vh;
-                transition:all ease 1s;
-                bottom:0;
-                top:100vh;
-                opacity:0;
-                font-size:0;
-                `;
-                setTimeout(()=>{
-                    document.querySelector('.openMenu').remove();
-
-                },300)
-    }
-}
-// menuFromLanding();
+            height:0vh;
+            transition:all ease 1s;
+            bottom:0;
+            top:100vh;
+            opacity:0;
+            font-size:0;
+            `;
+            setTimeout(()=>{
+                document.querySelector('.openMenu').remove();
+            },300)
+    }//menuClose;
+}// menuFromLanding();
